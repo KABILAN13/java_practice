c3a2efd60691b2a3c4ecaace3aea9e377b3af5eb
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-04 22:07:08 +0530

    Initial commit

---
+++ src/DateClass.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class DateClass {
+    public static void main(String[] args) {
+        int a=1;
+         Date today = new Date();
+         System.out.println(today);
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte a=127;
+        short b= 32000;
+        int c=2000000000;
+        long d=2000000000000000L;
+        float e=5.5F;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
