b2e514b24f3be89e52228326059d05800ad55ccd
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-04 22:14:31 +0530

    Initial commit

--- src/DateClass.java
+++ src/DateClass.java
@@ -3,6 +3,7 @@ public class DateClass {
     public static void main(String[] args) {
         int a=1;
         int b=2;
+        int c=3;
          Date today = new Date();
          System.out.println(today);
     }
