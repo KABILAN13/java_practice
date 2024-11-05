6469764ac60009d907aafbc50ce00a8a9933e328
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-05 20:26:17 +0530

    Initial commitd

---
+++ src/UnaryBinary.java
@@ -0,0 +1,12 @@
+public class UnaryBinary {
+    public static void main(String[] args) {
+      /* int a = 5;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);*/
+        int a = 5;
+        System.out.println(a++);
+        System.out.println(a);
+    }
+}
