b20f76a8e14cf319cce4740c87d81375326eb90f
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-05 20:17:56 +0530

    Initial commitd

--- src/Operators.java
+++ src/Operators.java
@@ -7,10 +7,12 @@ public class Operators {
         int d = a-b;
         int e = a*b;
         int f = a/b;
+        int g = b%a;
 
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
         System.out.println(f);
+        System.out.println(g);
     }
 }
