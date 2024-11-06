8d17a68aca141b040c2efff901cc984229c954aa
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-06 20:23:38 +0530

    Initial commitd

---
+++ src/SwitchCase.java
@@ -0,0 +1,19 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        char attendance='B';
+        switch (attendance){
+            case 'A':
+                System.out.println("No");
+                break;
+            case 'B':
+                System.out.println("Yes");
+                break;
+            case 'C':
+                System.out.println("No");
+                break;
+            default:
+                System.out.println("No");
+        }
+        System.out.println("ends");
+    }
+}
