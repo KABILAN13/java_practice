87f3231ef1c04b9421ad024c381af43045984425
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-06 20:15:37 +0530

    Initial commitd

---
+++ src/Condition.java
@@ -0,0 +1,21 @@
+public class Condition {
+    public static void main(String[] args) {
+        char attendance = 'A';
+        int total = 40;
+        if(attendance=='A')
+        {
+          if (total<=40);
+            {
+                System.out.println("class attended");
+            }
+        }
+        else if(attendance=='B')
+        {
+            System.out.println("not attended");
+        }
+        else{
+            System.out.println("not attended");
+        }
+        System.out.println("end");
+    }
+}
