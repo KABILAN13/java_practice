b8665b917197f7cc2e7795d3a14f2919da5a4111
KABILAN13 <117358756+KABILAN13@users.noreply.github.com>
2024-11-06 21:47:10 +0530

    Initial commitd

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,9 +1,9 @@
 public class ForLoop {
     public static void main(String[] args) {
         int n = 5;
-        int sum = 0;
+        int sum = 1;
         for (int i = 1; i <= n; i++) {
-            sum += i;
+            sum *= i;
         }
         System.out.println("sum of numbers: " + sum);
     }
